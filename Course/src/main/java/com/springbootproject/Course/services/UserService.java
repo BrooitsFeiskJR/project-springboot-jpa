7e89fb6a22f23240e5defad29e7d09232cc0d7a7
BrooitsFeiskJR <luizantonio0125@gmail.com>
2023-03-03 10:48:00 -0300

    User delete

--- Course/src/main/java/com/springbootproject/Course/services/UserService.java
+++ Course/src/main/java/com/springbootproject/Course/services/UserService.java
@@ -30,4 +30,8 @@ public class UserService {
     public User insert(User obj) {
         return userRepository.save(obj);
     }
+
+    public void delete(Long id) {
+        userRepository.deleteById(id);
+    }
 }
